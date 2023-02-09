92d91c24c4007312ad78c0e7b90fad028254a86c
Niranjan Hasabnis <niranjan.hasabnis@intel.com>
2023-02-09 08:55:33 -0800

    Adding a separate function to set TppLoweringConfig for Linalg ContractionOp
    Also, adding a command line switch to enable TPP lowering pipeline

--- compiler/src/iree/compiler/Codegen/LLVMCPU/KernelDispatch.cpp
+++ compiler/src/iree/compiler/Codegen/LLVMCPU/KernelDispatch.cpp
@@ -94,6 +94,11 @@ static llvm::cl::opt<bool> enableTripleTilingPipeline(
     llvm::cl::desc("enable triple tiling expert for matmul kernels"),
     llvm::cl::init(false));
 
+static llvm::cl::opt<bool> enableTppLoweringPipeline(
+    "iree-llvmcpu-enable-tpp-lowering-pipeline",
+    llvm::cl::desc("enable TPP based lowering passes for matmul and convolution kernels"),
+    llvm::cl::init(false));
+
 // Non-static options are used in other places.
 llvm::cl::opt<std::string> clCPUCodegenTransformDialectFileName(
     "iree-codegen-llvmcpu-use-transform-dialect",
@@ -735,10 +740,7 @@ static LogicalResult setMatmulPadRootConfig(
 
   return setOpConfigAndEntryPointFnTranslation(
       entryPointFn, op, tileSizes,
-      //DispatchLoweringPassPipeline::CPUDoubleTilingPadExpert);
-      // This is a hack currently.
-      // We need to define proper RootConfig function to invoke our Tpp->Xsmm->func lowering pipeline.
-      DispatchLoweringPassPipeline::CPUTppXsmm);
+      DispatchLoweringPassPipeline::CPUDoubleTilingPadExpert);
 }
 
 // Returns true if all the tiling sizes are divisible by the next level of
@@ -944,6 +946,16 @@ static SmallVector<int64_t> getMatmulWorkgroupSizes(func::FuncOp entryPointFn,
   LLVM_DEBUG(KD_DBGS() << "Matmul workgroup sizes: " << tileSizes << "\n");
 
   return tileSizes;
+}
+
+/// Sets the TPP based lowering configuration for dispatch region with root op
+/// that implements the contraction operation interface
+static LogicalResult setTppRootConfig(
+  func::FuncOp entryPointFn, linalg::ContractionOpInterface contractionOp) {
+  auto translationInfo = IREE::Codegen::TranslationInfoAttr::get(
+      entryPointFn.getContext(),
+      DispatchLoweringPassPipeline::CPUTppXsmm);
+  return setTranslationInfo(entryPointFn, translationInfo);
 }
 
 /// Sets the lowering configuration for dispatch region with root op that
@@ -1808,7 +1820,11 @@ static LogicalResult setRootConfigImpl(
         .Case<tensor::UnPackOp>(
             [&](auto op) { return setUnPackOpRootConfig(entryPointFn, op); })
         .Case<linalg::ContractionOpInterface>(
-            [&](auto op) { return setRootConfig(entryPointFn, op); })
+            [&](auto op) {
+              if (enableTppLoweringPipeline)
+                return setTppRootConfig(entryPointFn, op);
+              else
+                return setRootConfig(entryPointFn, op); })
         .Case<linalg::LinalgOp>(
             [&](auto op) { return setRootConfig(entryPointFn, op); })
         .Case<TilingInterface>(
