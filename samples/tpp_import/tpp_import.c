016d10c538ab45b5138268cd4d956722c118bd90
Niranjan Hasabnis <niranjan.hasabnis@intel.com>
2023-03-17 16:06:14 -0700

    Adding end-to-end tests for IREE-TPP integration

--- samples/tpp_import/tpp_import.c
+++ samples/tpp_import/tpp_import.c
@@ -9,26 +9,29 @@
 #include "iree/base/api.h"
 #include "iree/hal/local/executable_loader.h"
 
-extern int iree_xsmm_brgemm_dispatch_f32(void* context, void* params,
+extern int iree_xsmm_brgemm_dispatch(void* context, void* params,
                                          void* reserved);
 extern int iree_xsmm_matmul_dispatch(void* context, void* params,
                                          void* reserved);
 extern int iree_xsmm_unary_dispatch(void* context, void* params,
                                     void* reserved);
+extern int iree_xsmm_binary_dispatch(void* context, void* params,
+                                    void* reserved);
 
-extern int iree_xsmm_brgemm_invoke_f32(void* context, void* params,
+extern int iree_xsmm_brgemm_invoke(void* context, void* params,
                                        void* reserved);
 extern int iree_xsmm_matmul_invoke(void* context, void* params,
                                        void* reserved);
 extern int iree_xsmm_unary_invoke(void* context, void* params, void* reserved);
+extern int iree_xsmm_binary_invoke(void* context, void* params, void* reserved);
 
 static iree_status_t iree_samples_tpp_import_provider_resolve(
     void* self, iree_string_view_t symbol_name, void** out_fn_ptr,
     void** out_fn_context) {
   // Xsmm dispatch API.
   if (iree_string_view_equal(symbol_name,
-                             IREE_SV("xsmm_brgemm_dispatch_f32"))) {
-    *out_fn_ptr = iree_xsmm_brgemm_dispatch_f32;
+                             IREE_SV("xsmm_brgemm_dispatch"))) {
+    *out_fn_ptr = iree_xsmm_brgemm_dispatch;
     return iree_ok_status();
   }
   if (iree_string_view_equal(symbol_name,
@@ -40,10 +43,14 @@ static iree_status_t iree_samples_tpp_import_provider_resolve(
     *out_fn_ptr = iree_xsmm_unary_dispatch;
     return iree_ok_status();
   }
+  if (iree_string_view_equal(symbol_name, IREE_SV("xsmm_binary_dispatch"))) {
+    *out_fn_ptr = iree_xsmm_binary_dispatch;
+    return iree_ok_status();
+  }
 
   // Xsmm invoke API.
-  if (iree_string_view_equal(symbol_name, IREE_SV("xsmm_brgemm_invoke_f32"))) {
-    *out_fn_ptr = iree_xsmm_brgemm_invoke_f32;
+  if (iree_string_view_equal(symbol_name, IREE_SV("xsmm_brgemm_invoke"))) {
+    *out_fn_ptr = iree_xsmm_brgemm_invoke;
     return iree_ok_status();
   }
   if (iree_string_view_equal(symbol_name, IREE_SV("xsmm_matmul_invoke"))) {
@@ -52,6 +59,10 @@ static iree_status_t iree_samples_tpp_import_provider_resolve(
   }
   if (iree_string_view_equal(symbol_name, IREE_SV("xsmm_unary_invoke"))) {
     *out_fn_ptr = iree_xsmm_unary_invoke;
+    return iree_ok_status();
+  }
+  if (iree_string_view_equal(symbol_name, IREE_SV("xsmm_binary_invoke"))) {
+    *out_fn_ptr = iree_xsmm_binary_invoke;
     return iree_ok_status();
   }
   return iree_status_from_code(IREE_STATUS_NOT_FOUND);
